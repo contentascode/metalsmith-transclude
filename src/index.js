60e858c117baf22826737d27105d7cca8d29422f
Jun Matsushita <jun@iilab.org>
2018-01-13 16:31:41 +0100

    Do not transclude relative to sourcePath

--- src/index.js
+++ src/index.js
@@ -57,9 +57,9 @@ function plugin(options) {
           // If there is a sourcePath then we're in a nested transclusion
 
           // const relativePath = path.dirname(sourcePath);
-          // const targetKey = path.join(path.dirname(key), url);
-          const targetKey =
-            sourcePath !== 'string' ? path.join(path.dirname(sourcePath), url) : path.join(path.dirname(key), url);
+          const targetKey = path.join(path.dirname(key), url);
+          // const targetKey =
+          //   sourcePath !== 'string' ? path.join(path.dirname(sourcePath), url) : path.join(path.dirname(key), url);
 
           debug('>>> Using target key              :', targetKey);
           const resolvedKey = (files[targetKey] && targetKey) || (files[targetKey + '.md'] && targetKey + '.md');
